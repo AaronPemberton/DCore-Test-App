1940db3bf9c7fed7b349bbacbddd2d3648b94393
Aaron Pemberton <46750567+AaronPemberton@users.noreply.github.com>
2019-04-07 17:29:28 -0400

    Add files via upload

---
+++ DCore API Test/DCore API Test/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace DCore_API_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button_Test = new System.Windows.Forms.Button();
+            this.textBox_IP = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox_Parameter2 = new System.Windows.Forms.TextBox();
+            this.textBox_Parameter6 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_Parameter1 = new System.Windows.Forms.TextBox();
+            this.textBox_Parameter5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_Method = new System.Windows.Forms.TextBox();
+            this.textBox_Parameter4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_Parameter3 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.richTextBox_Method = new System.Windows.Forms.RichTextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.richTextBox_Response = new System.Windows.Forms.RichTextBox();
+            this.button_Execute = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_Test);
+            this.groupBox1.Controls.Add(this.textBox_IP);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(290, 49);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server IP";
+            // 
+            // button_Test
+            // 
+            this.button_Test.Location = new System.Drawing.Point(209, 17);
+            this.button_Test.Name = "button_Test";
+            this.button_Test.Size = new System.Drawing.Size(75, 23);
+            this.button_Test.TabIndex = 1;
+            this.button_Test.Text = "Test Connection";
+            this.button_Test.UseVisualStyleBackColor = true;
+            this.button_Test.Click += new System.EventHandler(this.button_Test_Click);
+            // 
+            // textBox_IP
+            // 
+            this.textBox_IP.Location = new System.Drawing.Point(6, 19);
+            this.textBox_IP.Name = "textBox_IP";
+            this.textBox_IP.Size = new System.Drawing.Size(197, 20);
+            this.textBox_IP.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_Parameter2);
+            this.groupBox2.Controls.Add(this.textBox_Parameter6);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.textBox_Parameter1);
+            this.groupBox2.Controls.Add(this.textBox_Parameter5);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.textBox_Method);
+            this.groupBox2.Controls.Add(this.textBox_Parameter4);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox_Parameter3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 67);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(290, 205);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Build API Call";
+            // 
+            // textBox_Parameter2
+            // 
+            this.textBox_Parameter2.Location = new System.Drawing.Point(80, 72);
+            this.textBox_Parameter2.Name = "textBox_Parameter2";
+            this.textBox_Parameter2.Size = new System.Drawing.Size(204, 20);
+            this.textBox_Parameter2.TabIndex = 5;
+            // 
+            // textBox_Parameter6
+            // 
+            this.textBox_Parameter6.Location = new System.Drawing.Point(80, 176);
+            this.textBox_Parameter6.Name = "textBox_Parameter6";
+            this.textBox_Parameter6.Size = new System.Drawing.Size(204, 20);
+            this.textBox_Parameter6.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Parameter 2:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 179);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Parameter 6:";
+            // 
+            // textBox_Parameter1
+            // 
+            this.textBox_Parameter1.Location = new System.Drawing.Point(80, 46);
+            this.textBox_Parameter1.Name = "textBox_Parameter1";
+            this.textBox_Parameter1.Size = new System.Drawing.Size(204, 20);
+            this.textBox_Parameter1.TabIndex = 3;
+            // 
+            // textBox_Parameter5
+            // 
+            this.textBox_Parameter5.Location = new System.Drawing.Point(80, 150);
+            this.textBox_Parameter5.Name = "textBox_Parameter5";
+            this.textBox_Parameter5.Size = new System.Drawing.Size(204, 20);
+            this.textBox_Parameter5.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Parameter 1:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Parameter 5:";
+            // 
+            // textBox_Method
+            // 
+            this.textBox_Method.Location = new System.Drawing.Point(80, 20);
+            this.textBox_Method.Name = "textBox_Method";
+            this.textBox_Method.Size = new System.Drawing.Size(204, 20);
+            this.textBox_Method.TabIndex = 1;
+            // 
+            // textBox_Parameter4
+            // 
+            this.textBox_Parameter4.Location = new System.Drawing.Point(80, 124);
+            this.textBox_Parameter4.Name = "textBox_Parameter4";
+            this.textBox_Parameter4.Size = new System.Drawing.Size(204, 20);
+            this.textBox_Parameter4.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Method:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Parameter 4:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Parameter 3:";
+            // 
+            // textBox_Parameter3
+            // 
+            this.textBox_Parameter3.Location = new System.Drawing.Point(80, 98);
+            this.textBox_Parameter3.Name = "textBox_Parameter3";
+            this.textBox_Parameter3.Size = new System.Drawing.Size(204, 20);
+            this.textBox_Parameter3.TabIndex = 7;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.richTextBox_Method);
+            this.groupBox3.Location = new System.Drawing.Point(308, 16);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(514, 179);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "C# Method";
+            // 
+            // richTextBox_Method
+            // 
+            this.richTextBox_Method.BackColor = System.Drawing.SystemColors.Desktop;
+            this.richTextBox_Method.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox_Method.Location = new System.Drawing.Point(6, 19);
+            this.richTextBox_Method.Name = "richTextBox_Method";
+            this.richTextBox_Method.Size = new System.Drawing.Size(502, 150);
+            this.richTextBox_Method.TabIndex = 0;
+            this.richTextBox_Method.Text = "";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.richTextBox_Response);
+            this.groupBox4.Location = new System.Drawing.Point(308, 201);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(514, 179);
+            this.groupBox4.TabIndex = 15;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "API Response";
+            // 
+            // richTextBox_Response
+            // 
+            this.richTextBox_Response.BackColor = System.Drawing.SystemColors.Desktop;
+            this.richTextBox_Response.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox_Response.Location = new System.Drawing.Point(6, 19);
+            this.richTextBox_Response.Name = "richTextBox_Response";
+            this.richTextBox_Response.Size = new System.Drawing.Size(502, 150);
+            this.richTextBox_Response.TabIndex = 1;
+            this.richTextBox_Response.Text = "";
+            // 
+            // button_Execute
+            // 
+            this.button_Execute.Location = new System.Drawing.Point(12, 351);
+            this.button_Execute.Name = "button_Execute";
+            this.button_Execute.Size = new System.Drawing.Size(75, 23);
+            this.button_Execute.TabIndex = 16;
+            this.button_Execute.Text = "Execute";
+            this.button_Execute.UseVisualStyleBackColor = true;
+            this.button_Execute.Click += new System.EventHandler(this.button_Execute_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 386);
+            this.Controls.Add(this.button_Execute);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(850, 425);
+            this.MinimumSize = new System.Drawing.Size(850, 425);
+            this.Name = "Form1";
+            this.Text = "DCore API Test";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button_Test;
+        private System.Windows.Forms.TextBox textBox_IP;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox_Parameter2;
+        private System.Windows.Forms.TextBox textBox_Parameter6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_Parameter1;
+        private System.Windows.Forms.TextBox textBox_Parameter5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_Method;
+        private System.Windows.Forms.TextBox textBox_Parameter4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_Parameter3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox richTextBox_Method;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RichTextBox richTextBox_Response;
+        private System.Windows.Forms.Button button_Execute;
+    }
+}
+
---
+++ DCore API Test/DCore API Test/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+
+namespace DCore_API_Test
+{
+    public partial class Form1 : Form
+    {
+        List<TextBox> paramboxes = new List<TextBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            paramboxes.Add(textBox_Parameter1);
+            paramboxes.Add(textBox_Parameter2);
+            paramboxes.Add(textBox_Parameter3);
+            paramboxes.Add(textBox_Parameter4);
+            paramboxes.Add(textBox_Parameter5);
+            paramboxes.Add(textBox_Parameter6);
+
+        }
+
+        private async void button_Test_Click(object sender, EventArgs e)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    var result = await httpClient.GetAsync(textBox_IP.Text);
+                    int StatusCode = (int)result.StatusCode;
+                    richTextBox_Response.Text = "Status Code: " + StatusCode;
+                }
+                catch
+                {
+                    richTextBox_Response.Text = "ERROR! Could not connect to specified URL.";
+                }
+            }
+
+        }
+
+
+
+        private void button_Execute_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(textBox_Method.Text))
+            {
+                richTextBox_Response.Text = "ERROR! No Method to test!";
+            }
+            else
+            {
+                List<string> parameters = new List<string>();
+                for (int i = 0; i < 6; i++)
+                {
+                    if (!string.IsNullOrWhiteSpace(paramboxes[i].Text))
+                    {
+                        parameters.Add(paramboxes[i].Text);
+                    }
+                }
+
+                displayCode(parameters);
+            }
+        }
+
+        private void displayCode(List<string> paramList)
+        {
+            string values = "";
+            if (paramList.Count != 0)
+            {
+                int x = 0;
+                values = ",\"params\":[";
+                foreach (string item in paramList)
+                {
+                    if (!item.Contains(','))
+                    {
+                        values = values + "\"" + item.Trim() + "\"";
+                    }
+                    else
+                    {
+                        string[] temp = item.Split(',');
+                        values = values + "[";
+                        foreach (string subitem in temp)
+                        {
+                            values = values + "\"" + subitem.Trim() + "\",";
+                        }
+                        values = values + "]";
+                        values = values.Remove(values.Length - 2, 1);
+                    }
+                    x++;
+                    if (x != paramList.Count)
+                    {
+                        values = values + ",";
+                    }
+                }
+                values = values + "]";
+            }
+
+            richTextBox_Method.Clear();
+            string tab = "  ";
+            richTextBox_Method.AppendText("private async Task<string> APICall_" + textBox_Method.Text + "()\n");
+            richTextBox_Method.AppendText("{\n");
+            richTextBox_Method.AppendText(tab + "using (var httpClient = new HttpClient())\n");
+            richTextBox_Method.AppendText(tab + "{\n");
+            richTextBox_Method.AppendText(tab + tab + "try\n");
+            richTextBox_Method.AppendText(tab + tab + "{\n");
+            richTextBox_Method.AppendText(tab + tab + tab + "using  (var request = new HttpRequestMessage(new HttpMethod(\"POST\"), \"");
+            richTextBox_Method.AppendText(textBox_IP.Text + "\"))\n");
+            richTextBox_Method.AppendText(tab + tab + tab + "{\n");
+            string format = values.Replace("\"", "\\\"");
+            richTextBox_Method.AppendText(tab + tab + tab + "string content = \"{\\\"jsonrpc\\\":\\\"2.0\\\",\\\"id\\\":1,\\\"method\\\":\\\"");
+            richTextBox_Method.AppendText(textBox_Method.Text + "\\\"" + format + "}\";\n");
+            richTextBox_Method.AppendText(tab + tab + tab + "request.Content = new StringContent(content, Encoding.UTF8, \"application/x-www-form-urlencoded\");\n");
+            richTextBox_Method.AppendText(tab + tab + tab + "var response = await httpClient.SendAsync(request);\n");
+            richTextBox_Method.AppendText(tab + tab + tab + "response.EnsureSuccessStatusCode();\n");
+            richTextBox_Method.AppendText(tab + tab + tab + "string responseBody = await response.Content.ReadAsStringAsync();\n");
+            richTextBox_Method.AppendText(tab + tab + tab + "return responseBody;\n");
+            richTextBox_Method.AppendText(tab + tab + tab + "}\n");
+            richTextBox_Method.AppendText(tab + tab + "}\n");
+            richTextBox_Method.AppendText(tab + tab + "catch\n");
+            richTextBox_Method.AppendText(tab + tab + "{\n");
+            richTextBox_Method.AppendText(tab + tab + tab + "return \"Error\";\n");
+            richTextBox_Method.AppendText(tab + tab + "}\n");
+            richTextBox_Method.AppendText(tab + "}\n");
+            richTextBox_Method.AppendText("}\n");
+
+            callAPI(values);
+        }
+
+        private async void callAPI(string parameterString)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    using (var request = new HttpRequestMessage(new HttpMethod("POST"), textBox_IP.Text))
+                    {
+                        string content = "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"" + textBox_Method.Text + "\"" + parameterString + "}";
+                        request.Content = new StringContent(content, Encoding.UTF8, "application/x-www-form-urlencoded");
+                        var response = await httpClient.SendAsync(request);
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        richTextBox_Response.Text = FormatJson(responseBody);
+                    }
+                }
+                catch
+                {
+                    richTextBox_Response.AppendText("An unknown error occured");
+                }
+            }
+        }
+
+        private const string INDENT_STRING = "    ";
+        static string FormatJson(string json)
+        {
+
+            int indentation = 0;
+            int quoteCount = 0;
+            var result =
+                from ch in json
+                let quotes = ch == '"' ? quoteCount++ : quoteCount
+                let lineBreak = ch == ',' && quotes % 2 == 0 ? ch + Environment.NewLine + String.Concat(Enumerable.Repeat(INDENT_STRING, indentation)) : null
+                let openChar = ch == '{' || ch == '[' ? ch + Environment.NewLine + String.Concat(Enumerable.Repeat(INDENT_STRING, ++indentation)) : ch.ToString()
+                let closeChar = ch == '}' || ch == ']' ? Environment.NewLine + String.Concat(Enumerable.Repeat(INDENT_STRING, --indentation)) + ch : ch.ToString()
+                select lineBreak == null
+                            ? openChar.Length > 1
+                                ? openChar
+                                : closeChar
+                            : lineBreak;
+
+            return String.Concat(result);
+        }
+    }
+}
